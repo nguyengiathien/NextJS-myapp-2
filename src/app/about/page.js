b15fd8608d64a4132b2b3823a20b0ea7eb59f01c
Nguyen Gia Thien <nguyengiathien2404@gmail.com>
2025-08-07 14:37:51 +0700

    bai2

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import styles from './about.module.css';
+export default function AboutPage(){
+    return(
+        <div className={styles.container}>
+            <h1 className={styles.title}>About Page</h1>
+            <p>This is the about page</p>
+        </div>
+    )
+}
